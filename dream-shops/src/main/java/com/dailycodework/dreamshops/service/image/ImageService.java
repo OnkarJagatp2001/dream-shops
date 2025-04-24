e51cda7693a61fbe8ffeaaa1c3e3d5f6c95e821b
onkar <onkarbjagtap2001@gmail.com>
2025-04-24 08:24:12 +0530

    Project setup done, Models of Category, Product, Images are done, and Services are implemented

---
+++ dream-shops/src/main/java/com/dailycodework/dreamshops/service/image/ImageService.java
@@ -0,0 +1,36 @@
+package com.dailycodework.dreamshops.service.image;
+
+import com.dailycodework.dreamshops.model.Image;
+import com.dailycodework.dreamshops.repository.ImageRepository;
+import com.dailycodework.dreamshops.service.product.IProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+@RequiredArgsConstructor
+public class ImageService implements IImageService {
+    private final ImageRepository imageRepository;
+    private final IProductService productService;
+
+    
+    @Override
+    public Image getImageById(Long id) {
+        return null;
+    }
+
+    @Override
+    public void deleteImageById(Long id) {
+
+    }
+
+    @Override
+    public Image saveImage(MultipartFile file, Long productId) {
+        return null;
+    }
+
+    @Override
+    public void updateImage(MultipartFile file, Long imageId) {
+
+    }
+}
